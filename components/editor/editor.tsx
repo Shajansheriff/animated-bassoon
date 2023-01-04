422e9c4d4a8878f166f30538adcf6b4d89064937
Shajahan A <Shajansheriff@users.noreply.github.com>
2023-01-04 13:20:54 +0530

    add editor

---
+++ components/editor/editor.tsx
@@ -0,0 +1,22 @@
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+function Placeholder() {
+  return <div className="editor-placeholder">Enter some plain text...</div>;
+}
+
+export default function CustomEditor() {
+  return (
+    <LexicalComposer
+      initialConfig={{ namespace: "CustomEditor", onError: console.error }}
+    >
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<Placeholder />}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+    </LexicalComposer>
+  );
+}
